04ec3d2ce2d0908a995a070c37f7bf7fc3eaa7ce
Madsen Servius <madsen@madsendynamics.com>
2020-12-24 13:44:23 -0800

    first commit

---
+++ app/seed.js
@@ -0,0 +1,25 @@
+
+const submissions = [
+    {
+        id:1,
+        title:'Yellow Pail',
+        description:'On-demand sand castle construction expertise.',
+        url:'#',
+        votes:16,
+        avatar:'../public/images/avatars/gromov.jpg',
+        submissionImage:'../public/images/submissions/image-yellow.png'
+    },
+    {
+        id:2,
+        title:'Hope for Haiti',
+        description:'We want to build future for Haiti.',
+        url:'#',
+        votes:12,
+        avatar:'../public/images/avatars/avatar.png',
+        submissionImage:'../public/images/submissions/haiti.png'
+    }
+]
+
+let Seed = {
+    submissions: submissions
+}
